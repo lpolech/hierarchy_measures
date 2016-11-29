178656be44d86eedb18f19bd72aa21f32ced64a6
toSterr <toSterr@gmail.com>
2016-11-29 13:52:07 +1100

    Enchance output when calculating measures for hierarhies separately

--- src/runner/ReadSeveralAndCalculateQualityMeasuresSeparately.java
+++ src/runner/ReadSeveralAndCalculateQualityMeasuresSeparately.java
@@ -145,6 +145,7 @@ public class ReadSeveralAndCalculateQualityMeasuresSeparately extends CommonRead
             System.out.println("Calculating..");
 
             for(int i = 0; i < args.length; i++) {
+                System.out.println("========= " + i + "/" + args.length + " =========");
                 String filePath = args[i];
                 saveBasicInfo(resultFilePath, filePath, useSubtree);
 
